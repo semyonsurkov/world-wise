160877756b9e5b3d6fb159c981fa38a1c6d3cddb
semyonsurkov <semyonsurkov@outlook.com>
2023-10-02 17:51:18 +0300

    Add basic structure for the project

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,14 @@
+import AppNav from '../components/AppNav';
+import Sidebar from '../components/Sidebar';
+
+import styles from './AppLayout.module.css';
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default AppLayout;
