29d84b4b075f383f6508ff49929cf9964fe91e00
semyonsurkov <semyonsurkov@outlook.com>
2023-10-10 23:43:20 +0300

    [ 216 ]Dynamic Routes With URL Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
 import CountriesList from './components/CountriesList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -48,6 +49,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} setIsLoading={setIsLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountriesList cities={cities} isLoading={isLoading} />}
